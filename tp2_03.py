c7fa2127c4d30e9654eedc675acc4799296dd569
Matias Alessandrello <matialess@gmail.com>
2024-09-29 12:35:50 -0300

    tp2 ejercicio 3

---
+++ tp2_03.py
@@ -0,0 +1,5 @@
+# Ejercicio 3: Dada una secuencia de caracteres, obtener dicha secuencia invertida.
+secuencia = input("Ingrese una secuencia de caracteres: ")
+secuencia_invertida = secuencia [::-1]
+
+print(secuencia_invertida)
